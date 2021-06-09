2902fc446acf6459296fb4897a3c2474f6e583d7
maxjacobzander <maxjacobzander@gmail.com>
2021-06-09 15:07:12 -0400

    Removed unnecessary line breaks and updated favorite tv shows

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,43 +13,21 @@ const IndexPage = () => (
         <div className="inner">
           <h3>A little background...</h3>
           <p>
-          As a young kid with glasses, people always assumed I knew my way 
-          around computers. I can’t even begin to tell you how many times people 
-          approached me in school or the library or wherever to ask for help with 
-          their computer problems. The bigger issue was that I would always figure 
-          out the solution, thus propelling the notion. Little did I know, I was 
-          discovering an affinity that would last a lifetime.
+          As a young kid with glasses, people always assumed I knew my way around computers. I can’t even begin to tell you how many times people approached me in school or the library or wherever to ask for help with their computer problems. The bigger issue was that I would always figure out the solution, thus propelling the notion. Little did I know, I was discovering an affinity that would last a lifetime.
           </p>
 
           <p>
-          Now, I’ve been borderline obsessed with the idea of creating something 
-          from nothing for as long as I can remember. From inventing things to 
-          writing music, creation has always been my life. The desire to make music 
-          took me to the Jacobs School of Music at Indiana University, where I 
+          Now, I’ve been borderline obsessed with the idea of creating something from nothing for as long as I can remember. From inventing things to writing music, creation has always been my life. The desire to make music took me to the Jacobs School of Music at Indiana University, where I 
           received both my Bachelor’s and Master’s degrees in Vocal Performance. 
-          I wrote music and did improv with IU’s longest-running improv troupe, 
-          creating original music and scenes every week; composed music for student 
-          films; and appeared onstage with IU Opera Theatre in sixteen different 
-          operas (singing in four different languages!). And after completing my 
-          Master’s, I dove straight into the world of professional music-making.
+          I wrote music and did improv with IU’s longest-running improv troupe, creating original music and scenes every week; composed music for student films; and appeared onstage with IU Opera Theatre in sixteen different operas (singing in four different languages!). And after completing my Master’s, I dove straight into the world of professional music-making.
           </p>
 
           <p>
-          When I look back, I guess I was always drawn to web and software development. 
-          In the fifth grade, I was one of my elementary school’s “Webmasters”, helping 
-          to build out an early version of the school’s website. When I entered the 
-          professional opera scene, I knew that I would need a website and built one using 
-          an online CMS (<a href="https://maxjacobzander.com" target="_blank" rel="noreferrer noopener">check it out here!</a>). When friends needed help with websites of their 
-          own, I was always happy to help. Eventually, I started teaching myself some HTML and 
-          CSS before deciding that I wanted to take things much more seriously.
+          When I look back, I guess I was always drawn to web and software development. In the fifth grade, I was one of my elementary school’s “Webmasters”, helping to build out an early version of the school’s website. When I entered the professional opera scene, I knew that I would need a website and built one using an online CMS (<a href="https://maxjacobzander.com" target="_blank" rel="noreferrer noopener">check it out here!</a>). When friends needed help with websites of their own, I was always happy to help. Eventually, I started teaching myself some HTML and CSS before deciding that I wanted to take things much more seriously.
           </p>
 
           <p>
-          After 6+ years working internationally as a professional opera singer, music teacher, 
-          and award-winning composer and arranger of music, I enrolled in Flatiron School’s full-time 
-          Full-Stack Software Engineering program, specializing in Ruby on Rails and React. Now, as a 
-          proud graduate of that program, I continue to learn every day to expand my toolbelt and build 
-          the best programs possible!
+          After 6+ years working internationally as a professional opera singer, music teacher, and award-winning composer and arranger of music, I enrolled in Flatiron School’s full-time Full-Stack Software Engineering program, specializing in Ruby on Rails and React. Now, as a proud graduate of that program, I continue to learn every day to expand my toolbelt and build the best programs possible!
           </p>
 
           <hr />
@@ -63,7 +41,7 @@ const IndexPage = () => (
               Favorite Color: <span style={{color: "blue"}}>Blue</span>
             </li>
             <li>
-              Favorite TV Shows: The Office, Parks & Rec, The Great British Bake Off, The Big Flower Fight, Law & Order SVU, Criminal Minds
+              Favorite TV Shows: The Office, Parks & Rec, Schitt's Creek, The Great British Bake Off, The Big Flower Fight, Law & Order SVU, Criminal Minds
             </li>
             <li>
               Coding Languages: <strong>Ruby, JavaScript, SQL, HTML, CSS</strong>
